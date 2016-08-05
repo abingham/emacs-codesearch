ee7c1b506ed18c2602a95ef58ed3bb5948d90d08
Austin Bingham <austin@sixty-north.com>
2016-08-05 13:43:02 +0200

    Sorta seems to work.

--- listing-codesearch.el
+++ listing-codesearch.el
@@ -62,6 +62,90 @@
 
 (require 'codesearch)
 
+(defgroup listing-codesearch nil
+  "Variables related to listing-codesearch."
+  :prefix "listing-codesearch-"
+  :group 'tools)
+
+(defface listing-codesearch-filename
+  '((t :inherit font-lock-constant-face))
+  "Face used to highlight filenames in matches."
+  :group 'listing-codesearch)
+
+(defface listing-codesearch-line-number
+  '((t :inherit font-lock-keyword-face))
+  "Face used to highlight line numbers in matches."
+  :group 'listing-codesearch)
+
+(defconst listing-codesearch--match-regex "^\\(.*\\):\\([0-9]+\\):"
+  "The regular expression used to find matches in the codesearch output.")
+
+(define-button-type 'listing-codesearch--filename-match-button
+  'face 'listing-codesearch-filename
+  'follow-link 't
+  'button 't)
+
+(define-button-type 'listing-codesearch--line-number-match-button
+  'face 'listing-codesearch-line-number
+  'follow-link 't
+  'button 't)
+
+(defun listing-codesearch--make-filenames-clickable (buff)
+  "Finds all codesearch matches in BUFF, turning them into
+clickable buttons that link to the matched file/line-number.
+
+BUFF is assumed to contain the output from running csearch.
+"
+  (with-current-buffer buff
+    (beginning-of-buffer)
+    (while (re-search-forward listing-codesearch--match-regex nil t)
+      (lexical-let* ((filename (match-string 1))
+                     (line-number (string-to-number (match-string 2)))
+                     (visit-match (lambda (b)
+                                    (find-file-other-window filename)
+                                    (goto-line line-number))))
+        (make-text-button
+         (match-beginning 1)
+         (match-end 1)
+         'type 'listing-codesearch--filename-match-button
+         'action visit-match)
+
+        (make-text-button
+         (match-beginning 2)
+         (match-end 2)
+         'type 'listing-codesearch--line-number-match-button
+         'action visit-match)))))
+
+(defvar listing-codesearch-pattern-history nil)
+
+(defvar listing-codesearch-file-pattern-history nil)
+
+;;;###autoload
+(defun listing-codesearch-search (pattern file-pattern)
+  "Search files matching FILE-PATTERN in the index for PATTERN."
+  (interactive
+   (list
+    (read-string "Pattern: " (thing-at-point 'symbol)
+                 'listing-codesearch-pattern-history (car listing-codesearch-pattern-history))
+    (read-string "File pattern: " ".*"
+                 'listing-codesearch-file-pattern-history (car listing-codesearch-file-pattern-history))))
+  (let ((switch-to-visible-buffer t)
+        (buff (get-buffer-create "*codesearch-results*"))
+        (file-pattern (if (memq system-type '(windows-nt ms-dos))
+                          (replace-regexp-in-string "/" "\\\\\\\\" file-pattern)
+                        file-pattern)))
+    (with-current-buffer buff
+      (read-only-mode 0)
+      (erase-buffer)
+      (codesearch-run-csearch
+       (lambda (result)
+         (insert result)
+         (listing-codesearch--make-filenames-clickable (current-buffer))
+         (pop-to-buffer (current-buffer)))
+       default-directory
+       "-f" file-pattern
+       "-n" pattern))))
+
 (defun listing-codesearch--handle-listing (results)
   (with-current-buffer "*codesearch-directories*"
     (let ((dirs (-slice (split-string results "\n") 0 -1)))
